f471f6b8a93c71ef11ce094970341f4adf831fd2
mdube99 <mjdube99@gmail.com>
2024-05-13 01:34:59 +0000

    updated

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -16,8 +16,7 @@
       pkgs.tmuxPlugins.better-mouse-mode
       pkgs.tmuxPlugins.sensible
       pkgs.tmuxPlugins.vim-tmux-navigator
-      # pkgs.tmuxPlugins.power-theme
-      pkgs.tmuxPlugins.catppuccin
+      pkgs.tmuxPlugins.power-theme
       pkgs.tmuxPlugins.fuzzback
     ];
 
