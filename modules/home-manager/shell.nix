61c400c1c84e9d6697bd5c7d6a76f340852340aa
mdube99 <mjdube99@gmail.com>
2024-05-15 00:42:12 +0000

    updated

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -11,27 +11,33 @@
     autocd = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    history.size = 10000;
-    history.save = 10000;
-    history.expireDuplicatesFirst = true;
-    history.ignoreDups = true;
-    history.ignoreSpace = true;
+    history = {
+      size = 10000;
+      save = 10000;
+      expireDuplicatesFirst = true;
+      ignoreDups = true;
+      ignoreSpace = true;
+    };
     historySubstringSearch.enable = true;
 
     envExtra = ''
       # Custom cd
-            c() {
+        c() {
       	  cd $1;
       	  lsd;
-            }
+        }
+        dvd() {
+          echo "use flake github:the-nix-way/dev-templates?dir=$1" >> .envrc;
+          direnv allow;
+        }
 
       # Courtesy of Graham Helton
       # e.g. cp `DL` .
-            DL() {
+        DL() {
       	  variable1=$(echo ~/Downloads/$(/usr/bin/ls -ltr ~/Downloads | tail -n1 | awk {"print \$9"}));
       	  echo $variable1;
-            }
-            eval "$(direnv hook zsh)"
+        }
+        eval "$(direnv hook zsh)"
     '';
 
     initExtra = "source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh && any-nix-shell zsh --info-right | source /dev/stdin";
@@ -52,6 +58,7 @@
       activate = "source env/bin/activate";
       IP = "tmux list-windows | grep \"\*\" | grep -E -o \"([0-9]{1,3}[\.]){3}[0-9]{1,3}\"";
       t = "sesh connect $(sesh list | fzf)";
+      #dvd = "echo use flake \"github:the-nix-way/dev-templates?dir=$1\" >> .envrc && direnv allow";
 
       # script to copy nmap scripts
       nmapScripts = "ls /usr/share/nmap/scripts | fzf --reverse | xsel --clipboard";
