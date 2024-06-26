7fd89ec164479dacf7e80bd5a4135184e9007252
mdube99 <mjdube99@gmail.com>
2024-06-26 18:00:13 -0500

    updated

--- flake.nix
+++ flake.nix
@@ -40,13 +40,6 @@
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
-
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = {
