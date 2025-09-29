40b83fb4de498092c084be7f9eccae158898d2f0
mdube99 <mjdube99@gmail.com>
2025-09-29 16:51:33 -0500

    no longer shows lazy news

--- configurations/nvim/lua/config/lazy.lua
+++ configurations/nvim/lua/config/lazy.lua
@@ -5,11 +5,19 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-
 require('lazy').setup {
   spec = {
     -- add LazyVim and import its plugins
-    { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
+    {
+      'LazyVim/LazyVim',
+      import = 'lazyvim.plugins',
+      opts = {
+        news = {
+          lazyvim = false,
+          neovim = false,
+        },
+      },
+    },
     -- import any extras modules here
     { import = 'lazyvim.plugins.extras.lang.docker' },
     { import = 'lazyvim.plugins.extras.lang.clangd' },
@@ -32,6 +40,7 @@ require('lazy').setup {
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+
   -- install = { colorscheme = { 'tokyonight', 'habamax' } },
   checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
